10ab85873fe7462890efb72981eacb181ba74cda
Lenovo <s.kumoreq@outlook.com>
2025-10-07 18:13:59 +0200

    zaimportowane biblioteki w Lotto

--- Lab1/Lotto.java
+++ Lab1/Lotto.java
@@ -1,4 +1,10 @@
-package Lab1;
+import java.util.Random;
+import java.util.HashSet;
+import java.util.Set;
 
 public class Lotto {
+    public static void main(String[] args) {
+        Random random = new Random();
+        System.out.println(random.nextInt(49));
+    }
 }
