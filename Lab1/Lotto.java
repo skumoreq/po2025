fed026474b51776f26ccffaddc1973c90858e722
Lenovo <s.kumoreq@outlook.com>
2025-10-09 01:57:36 +0200

    skończono klasę lotto

--- Lab1/Lotto.java
+++ Lab1/Lotto.java
@@ -1,10 +1,14 @@
 import java.util.Random;
 import java.util.HashSet;
-import java.util.Set;
 
 public class Lotto {
     public static void main(String[] args) {
         Random random = new Random();
-        System.out.println(random.nextInt(49));
+        HashSet<Integer> set = new HashSet<>();
+        while (set.size() < 6) {
+            int number = random.nextInt(49) + 1;
+            set.add(number);
+        }
+        System.out.println(set);
     }
 }
